b23864be374e220ee1af9ae5be9ad91149a7beeb
sasha walker <sashawalker31@gmail.com>
2020-01-29 03:14:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name="Arel")
+    return "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(attendees) 
+  attendees=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
+  attendees.each {|val| 
+  puts "Hello, my name is #{badges}."}
+  puts "Hello, my name is #{badges}."
+
+  
+def assign_rooms 
+  names=["steve"]
+  counter=1
+  names.each_with_index { |name| }
+  counter +=1
+    return "Hello, #{name}! You'll be assigned to room #{room}!"
+    
+end
